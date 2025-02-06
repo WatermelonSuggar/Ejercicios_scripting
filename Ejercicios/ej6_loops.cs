d5042c6b161c6badad71742a9f8f9cfb901d05a6
equintero88 <147670537+equintero88@users.noreply.github.com>
2025-02-06 15:13:38 -0500

    correción TryParse

--- Ejercicios/ej6_loops.cs
+++ Ejercicios/ej6_loops.cs
@@ -1,8 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Taller_1
 {
@@ -10,25 +6,30 @@ namespace Taller_1
     {
         static void Main()
         {
-            Console.WriteLine("Escoja el número de numeros impares a sumar: ");
-            int sumNums = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(sumNums);
+            Console.WriteLine("Escoja el número de números impares a sumar: ");
+            
+
+            int sumNums;
+            while (!int.TryParse(Console.ReadLine(), out sumNums) || sumNums <= 0)
+            {
+                Console.WriteLine("Ingrese un número entero positivo válido:");
+            }
 
             int n = 1;
-            int Suma = 0;
+            int suma = 0;
             int count = 0;
             
-           do
-           {
-              Suma += n;
-              n += 2;
-              count++;
-           }
-           while (count <  sumNums);
-           Console.WriteLine("Total de la suma de los primeros " + sumNums + " números impares: " + Suma);
+            do
+            {
+                suma += n;
+                n += 2;
+                count++;
+            }
+            while (count < sumNums);
+            
+            Console.WriteLine($"Total de la suma de los primeros {sumNums} números impares: {suma}");
         }
-        
-
-
     }
 }
+
+//  out sumNums -> Esta expresión le permite al método TryParse asignarle un valor 'sumNums'. La expresión 'out' se asegura de que una variable no quede sin inicializar
